3e3ad5bfb800b726f2931c38c59db4f9839ef2e8
Domino419 <noris419@naver.com>
2025-02-06 00:32:08 +0900

    propType 실습 end

--- Ch03/hello-ch03/src/App.js
+++ Ch03/hello-ch03/src/App.js
@@ -2,8 +2,9 @@ import MyComponent from "./MyComponent";
 
 const App =() => {
 //  return <MyComponent name ="홍길동" test =" 테스트" /> ;
+    console.log("::::::::::::::::::::::::: 실행 환경 : " ,  process.env.NODE_ENV);
     console.log("::::::::::::::::::::::::::::::::::::::::: App.js ");
-    return <MyComponent /> ;
+    return <MyComponent> 리액트 </MyComponent>
 };
 
 export default App ;
--- Ch03/hello-ch03/src/MyComponent.js
+++ Ch03/hello-ch03/src/MyComponent.js
@@ -1,31 +1,27 @@
-const MyComponent = ({ a = "기본이름", b = "테스트BBB", c = "테스트cccc" }) => {
-    console.log("::::::::::::::::::::::::::::::::::::::::: MyComponent.js");
-    console.log("Props 값 확인:", { a, b, c });
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름'
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+  };
+  render() {
+    const { name, favoriteNumber, children } = this.props; // 비구조화 할당
+    console.log("::::::::::::::::::::::::::::::::::::::::: MyComponent.js ");
     return (
-        <div> 
-            안녕하세요? 제 이름은 {a} 입니다. 
-        </div>
-    ); 
-};
+      <div>
+        안녕하세요, 제 이름은 {name} 입니다. <br />
+        children 값은 {children}
+        입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+      </div>
+    );
+  }
+}
 
 export default MyComponent;
-
-
-
-// const MyComponent = props => {
-//     console.log("::::::::::::::::::::::::::::::::::::::::: MyComponent.js 111");
-//     console.log("Props 값 확인:", props);
-//     return <div> 안녕하세요???  제 이름은 {props.a} 입니다.   체크 : {props.b}  , 체크 {props.c}   </div> ; 
-// }; 
-
-// MyComponent.defaultProps = {
-//     a : '기본이름' ,
-//     b : `bbb` ,
-//     c : "cccc" 
-// } ;
-
-
-
-//  // 다른 파일에서 이 파일을 import할 때 위에서 선언한 MyComponent를 불러오도록 설정하는 부분 
-// export default MyComponent ;  
