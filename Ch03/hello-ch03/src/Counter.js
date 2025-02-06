06cc0298dc041e40d4ba205131a162ad0463953a
Domino419 <noris419@naver.com>
2025-02-06 23:57:39 +0900

    .4.11

--- Ch03/hello-ch03/src/Counter.js
+++ Ch03/hello-ch03/src/Counter.js
@@ -1,26 +1,28 @@
-// 3.4.1 클래스형 컴포넌트의 state
+// 3.4.11 state 객체 안에 여러 값이 있을 때 
 
 import  { Component } from 'react';
+
 
 class Counter extends Component {
     constructor(props) {
         super(props)  ; 
             // state의 초기값 설정 
         this.state ={
-            number : 0 
+            number : 0 , 
+            fixedNumber : 0
          };
     }
     render() {
-        const { number } =this.state ; // state 를 조회할 때는 this.state로 조회
-
+        const { number, fixedNumber } =this.state ; // state 를 조회할 때는 this.state로 조회
         console.log("::::::::::::::::::::::::::::::::::::::::: Counter.js ");
-
         return (
-            <div>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>   
                 <h1> {number} </h1>
+                <h2> 바뀌지 않는 값 : {fixedNumber}</h2>
                 <button
                 // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정 
                 onClick={() => {
+                    console.log("::::::::::::::::::::::::::::::::::::::::: number : " , {number,fixedNumber });
                     this.setState({ number:number+1}) ; 
                 }}
                 >
