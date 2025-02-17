5be9f130f36e301b65f25f6b1df83c48d98ac01a
Domino419 <noris419@naver.com>
2025-02-17 17:56:46 +0900

    chap10. 일정관리 웹 어플리케이션 만들기

---
+++ Ch10/todo-app/src/App.js
@@ -0,0 +1,8 @@
+const App = () => {
+  console.log("::::::::::::::::::::::::: Ch10.일정관리 웹 어플리케이션 만들기 : ", process.env.NODE_ENV)
+  return (
+    <div> Todo App 을 만들자 ! </div>
+  )
+}
+
+export default App;
---
+++ Ch10/todo-app/src/component/TodoTemplate.js
@@ -0,0 +1,11 @@
+import './TodoTemplate.scss'
+
+const TodoTemplate = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default TodoTemplate;
