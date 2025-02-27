34e86433f6c2ce6d4c82e7641dfe3aeccbfc9fd5
Domino419 <noris419@naver.com>
2025-02-27 23:46:24 +0900

    chap13. header 적용

--- Ch13/router-tutorial/src/App.js
+++ Ch13/router-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 //import About from './pages/About';
+import Layout from './Layout';
 import AboutQueryString from './pages/AboutQueryString'; // 13.4.1 추가
 import Home from './pages/Home';
 import Profile from './pages/Profile';
@@ -10,20 +11,21 @@ import Article from './pages/Article';
 const App = () => {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutQueryString" element={<AboutQueryString />} />
-          <Route path="/profiles/:username" element={<Profile />} />
-          <Route path="/articles" element={<Articles />}>
+    <div>
+    <Routes>
+        <Route element={<Layout />} >
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutQueryString" element={<AboutQueryString />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
+        <Route path="/articles" element={<Articles />}>
             <Route path=":id" element={<Article />} />
           </Route>
-        </Routes>
-      </div>
+    </Routes>
+    </div>
     </Router>
-  )
-    ;
-};
+  );
+}
 
 export default App;
 /*
