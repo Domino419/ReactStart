32594c1b24a48c92ec72b7c985ebb517ed46c256
Domino419 <noris419@naver.com>
2025-03-01 23:35:51 +0900

    chap14.외부 api를 연동하여 뉴스 뷰어 만들기.

---
+++ Ch14/news-viewer/src/App.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import axios from 'axios' ; 
+
+
+const App = () => {
+  const [data, setData ] = useState(null) ; 
+  const onClick = () => {
+    axios.get(`https://jsonplaceholder.typicode.com/todos/1`).then(Response => {
+      setData(Response.data) ; 
+    }); 
+  } ; 
+
+  return (
+    <div>
+      <button onClick={onClick}> 불러오기 </button>
+    { data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/> }
+    </div>
+  );
+};
+
+export default App;
