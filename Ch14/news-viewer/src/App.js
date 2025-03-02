d074068a96c06208ef8b0cc37ae588ce00edad62
Domino419 <noris419@naver.com>
2025-03-03 02:04:02 +0900

    chap14.외부에서 API를 연동하여 뉴스 뷰어 만들기 ing~

--- Ch14/news-viewer/src/App.js
+++ Ch14/news-viewer/src/App.js
@@ -1,13 +1,38 @@
+import NewsList from './components/NewsList';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <NewsList />
+    </div>
+  );
+};
+
+export default App;
+
+
+/*
 import {useState} from 'react';
 import axios from 'axios' ; 
 
 
 const App = () => {
   const [data, setData ] = useState(null) ; 
-  const onClick = () => {
-    axios.get(`https://jsonplaceholder.typicode.com/todos/1`).then(Response => {
+  const onClick = async () => {
+    console.log('async !! ')
+    try{
+      console.log('try !! ')
+      const Response = await axios.get(
+        // `https://jsonplaceholder.typicode.com/todos/1`
+        // `https://newsapi.org/v2/top-headlines?country=kr&apiKey=937ec5a8aaa24aba94d4ef488637c221`
+        `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=937ec5a8aaa24aba94d4ef488637c221`
+      ) ;
       setData(Response.data) ; 
-    }); 
+    } catch (e) {
+      console.log(e) ; 
+    }
   } ; 
 
   return (
@@ -19,3 +44,4 @@ const App = () => {
 };
 
 export default App;
+*/
