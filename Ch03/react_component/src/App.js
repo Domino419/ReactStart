b1aeb2f1696395980cb276cd18d9a0ac392e1a80
noris419 <noris419@naver.com>
2026-06-02 16:40:20 +0900

    ch03.컴포넌트 end

--- Ch03/react_component/src/App.js
+++ Ch03/react_component/src/App.js
@@ -1,13 +1,21 @@
 import MyComponent from "./MyComponent";
 import MyComponentClass from "./MyComponentClass";
 import Counter from "./Counter";
+import Say2p from "./Say2p";
 
 const App = () => {
  // return <MyComponent name="React" favoriteNumber={1}> MyComponent </MyComponent> 
  // return <MyComponentClass name="React" favoriteNumber={1}> MyComponentClass </MyComponentClass> 
  
  /* 3.4.1 클래스형 컴포넌트의 state */ 
- return <Counter /> 
+ //return <Counter /> 
+
+ /* 3.4.2 useState 사용하기 */
+//return <Say /> 
+
+ /* 3.4.2.3 한 컴포넌트에서 useState 여러번 사용하기 */
+return <Say2p /> 
+
 
 }
 
