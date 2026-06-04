bf0023da6b32fcd50e385e9c61286ab29fa2375b
noris419 <noris419@naver.com>
2026-06-04 10:20:05 +0900

    ch04.이벤트 핸들링 실습 end

--- Ch04/eventhandling/src/EventPractice.js
+++ Ch04/eventhandling/src/EventPractice.js
@@ -1,47 +1,73 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class EventPractice extends Component {
-  state = {
-    messega: "",
-  };
+const EventPractice = () =>  {
+console.log("EventPractice 호출 ---")
 
-  constructor(props) {
-    super(props) ; 
-    this.handleChange = this.handleChange.bind(this) ;
-    this.handleClick = this.handleClick.bind(this) ;
+ const  [ form, setForm ]   = useState({
+  username : '', 
+  message : '' , 
+ }) ;
+
+ const { username , message  } = form ; 
+
+ const onChange = e => {
+  console.log("현재 form 값:", form);
+ //console.log("onChange 호출 ---")
+  const nextForm = {
+    ...form,                          // 기존의 form 내용을 이 자리에 복사한 뒤 
+    [e.target.name ] : e.target.value // 원하는 값을 덮어 씌우기 
+  } ; 
+  setForm (nextForm) ; 
+ } ; 
+
+
+ const onClick = () => {
+console.log("onClick 호출 ---")
+  alert ( username + ' : ' + message ) ; 
+  setForm ({
+    username: '', 
+    message: '' 
+  });
+ };
+
+ const onKeyPress = e =>{
+  console.log("onKeyPress 호출 ---")
+  if (e.key === 'Enter') {
+    onClick( ) ; 
   }
+ } ; 
 
-  handleChange(e) {
-    this.setState ({
-        messega: e.target.value
-    }) ; 
-  }
- 
-   handleClick() {
-    alert(this.state.messega) ; 
-    this.setState({
-        messega: ''
-    }) ; 
-  }
-
-
-  render() {
     return (
       <div>
         <h1> 이벤트 연습 </h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명 "
+          value={username}
+          onChange={onChange} 
+        />
 
         <input
           type="text"
           name="message"
           placeholder="아무거나 입력해보세요"
-          value={this.state.messega}
-          onChange={this.handleChange} 
-        ></input>
+          value={message}
+          onChange={onChange} 
+          onKeyPress={onKeyPress}
+        />
 
-        <button onClick={this.handleClick}> 확인 </button>
+        <button onClick={onClick}> 확인 </button>
       </div>
     );
-  }
-}
+} ;
 
 export default EventPractice;
+
+
+/* 
+ 대괄호, 중괄호 헷갈릴 때는 데이터가 배열인지 객체인지를 먼저 확인. 
+ 
+ 베열 : const [a, b] = [1, 2];	배열은 순서(index) 기반으로 값을 꺼냄
+ 객체 :	const {x, y} = {x: 1, y: 2};	객체는 키(key) 이름 기반으로 값을 꺼냄
+ */ 
