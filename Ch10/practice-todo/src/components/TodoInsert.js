4d98cc9ff9f6d3cb5d61e3e796c7c317b6bfc546
noris419 <noris419@naver.com>
2026-06-05 17:06:35 +0900

    일정추가 기능 구현

--- Ch10/practice-todo/src/components/TodoInsert.js
+++ Ch10/practice-todo/src/components/TodoInsert.js
@@ -1,20 +1,43 @@
 import { MdAdd } from 'react-icons/md';
-import './TodoInsert.scss'
+import './TodoInsert.scss';
+import { useCallback, useState } from 'react';
 
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
 
-const TodoInsert = props => {
-    return (
-        <form className="TodoInsert">
-            <input placeholder='할 일을 입력하세요 '></input>
-            <button type='submit'>
-                <MdAdd/>
-            </button>
-        </form>
-    );
+  const onChange = useCallback((e) => {
+    console.log ('onChange 함수 동작 확인 ')
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    e => {
+        onInsert(value) ; 
+        setValue('') ; // value값 초기화 
+
+        // submit 이벤트는 브라우저에서 새로고침을 발생 시키기 때문에 
+        // 방지하기 위해서 이 함수를 호출해야함
+
+        e.preventDefault();
+    }, 
+    [onInsert, value] ,
+  ); 
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요 "
+        value={value}
+        onChange={onChange}
+      />
+
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
 };
 
-TodoInsert.propTypes = {
-    
-};
+TodoInsert.propTypes = {};
 
 export default TodoInsert;
--- Ch10/practice-todo/src/components/TodoTemplate.js
+++ Ch10/practice-todo/src/components/TodoTemplate.js
@@ -3,7 +3,7 @@ import './TodoTemplate.scss' ;
 const TodoTemplate = ({children}) => {
     return (
         <div className='TodoTemplate'>
-            <dic className="app-title">일정 관리 </dic>
+            <div className="app-title">일정 관리 </div>
             <div className='content'>{children}</div>
         </div>
     );
