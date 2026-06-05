8844b3d470fa9634bea991d79a71881fa769e0b6
noris419 <noris419@naver.com>
2026-06-05 13:36:36 +0900

    classnames 사용하기

--- Ch09/practice-styling/src/App.js
+++ Ch09/practice-styling/src/App.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
 import SassComponent from "./SassComponent";
+import CSSModule  from "./CSSModule";
 
 class App extends Component {
   render() {
     return (
-      <SassComponent/>
+      <CSSModule/>
     )
   }
 }
