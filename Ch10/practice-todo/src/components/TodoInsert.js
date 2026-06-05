cb7abcb98adb99ab671a6819f1ef3239e67b32ee
noris419 <noris419@naver.com>
2026-06-05 14:51:56 +0900

    Todo insert js 작업 중

---
+++ Ch10/practice-todo/src/components/TodoInsert.js
@@ -0,0 +1,20 @@
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss'
+
+
+const TodoInsert = props => {
+    return (
+        <form className="TodoInsert">
+            <input placeholder='할 일을 입력하세요 '></input>
+            <button type='submit'>
+                <MdAdd/>
+            </button>
+        </form>
+    );
+};
+
+TodoInsert.propTypes = {
+    
+};
+
+export default TodoInsert;
