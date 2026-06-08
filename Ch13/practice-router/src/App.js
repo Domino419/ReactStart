515536ac4b0b83a3ff173cbe2604a2bec429383c
noris419 <noris419@naver.com>
2026-06-08 14:53:45 +0900

    header 설정 end

--- Ch13/practice-router/src/App.js
+++ Ch13/practice-router/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Layout from "./Layout";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
@@ -8,11 +9,13 @@ import Article from "./pages/Article";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/Profiles/:username" element={<Profile/>} />
-      <Route path="/articles" element={<Articles /> }>
-      < Route path=":id" element={<Article />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
       </Route>
     </Routes>
   );
