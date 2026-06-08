e6574941e3f7f983ae7f48b30e90b150aaf7d58a
noris419 <noris419@naver.com>
2026-06-08 15:14:25 +0900

    ch13.리액트 라우터로 SPA 개발하기. end

--- Ch13/practice-router/src/App.js
+++ Ch13/practice-router/src/App.js
@@ -5,6 +5,10 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import NotFound from "./pages/NotFound";
+import MyPage from "./pages/MyPage";
+import Login  from "./pages/Login";
+
 
 function App() {
   return (
@@ -17,6 +21,9 @@ function App() {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/mypage" element={<MyPage />}/>   
+      <Route path="*" element={<NotFound />}/>
     </Routes>
   );
 }
