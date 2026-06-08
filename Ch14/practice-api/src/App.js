118bde3b07397d46a75f43caf55bbd0d9e8358a0
noris419 <noris419@naver.com>
2026-06-08 16:57:26 +0900

    newsitem 페이지 작업

--- Ch14/practice-api/src/App.js
+++ Ch14/practice-api/src/App.js
@@ -1,23 +1,32 @@
 import { useState } from 'react';
-import axios, { Axios } from 'axios';
-
+import axios  from 'axios';
 
 const App = () => {
+  const [data, setData] = useState(null);
 
-  const [data, setData] = useState(null) ; 
-
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(Response => {
-      setData(Response.data)  ; 
-    }) ; 
-  }
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/everything?q=tesla&from=2026-05-08&sortBy=publishedAt&apiKey=937ec5a8aaa24aba94d4ef488637c221'
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div>
       <div>
         <button onClick={onClick}>불러오기 </button>
       </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2 )} readOnly={true}/> }
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
 };
