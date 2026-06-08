6a36a40870411a33d8fb3928610a5283e1b6fef0
noris419 <noris419@naver.com>
2026-06-08 15:26:21 +0900

    ch14. api 실습 프로젝트 생성

---
+++ Ch14/practice-api/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import axios, { Axios } from 'axios';
+
+
+const App = () => {
+
+  const [data, setData] = useState(null) ; 
+
+  const onClick = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(Response => {
+      setData(Response.data)  ; 
+    }) ; 
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기 </button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2 )} readOnly={true}/> }
+    </div>
+  );
+};
+
+export default App;
