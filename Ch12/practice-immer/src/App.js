45d026cf03c2bd3fbc0c6a94066286ce57a1adaf
noris419 <noris419@naver.com>
2026-06-08 13:23:48 +0900

    APP 컴포넌트에 immer 적용

--- Ch12/practice-immer/src/App.js
+++ Ch12/practice-immer/src/App.js
@@ -1,58 +1,68 @@
-import { useRef, useCallback, useState } from 'react';
+import { useRef, useCallback, useState } from "react";
+import { produce } from "immer";
 
 const App = () => {
   const nextId = useRef(1);
-  const [form, setForm] = useState({ name: '', username:  ''});
+  const [form, setForm] = useState({ name: "", username: "" });
   const [data, setData] = useState({
     array: [],
-    uselessValue: null
+    uselessValue: null,
   });
 
   // input 수정 함수
   const onChange = useCallback(
-    e => {
-      const {name, value} = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
+    (e) => {
+      const { name, value } = e.target;
+      setForm(
+        produce(form, (draft) => {
+          draft[name] = value;
+        }),
+      );
     },
     [form],
+    console.log("----onChange :::::"),
   );
 
   // from 등록 함수
   const onSubmit = useCallback(
-    e => {
+    (e) => {
+      console.log("----onSubmit :::::");
       e.preventDefault();
       const info = {
         id: nextId.current,
         name: form.name,
-        username: form.username
+        username: form.username,
       };
 
       // array 에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        }),
+      );
 
       //form 초기화
       setForm({
-        name: '',
-        username: '',
+        name: "",
+        username: "",
       });
       nextId.current += 1;
     },
-    [data, form.name, form.username ]
+    [data, form.name, form.username],
   );
 
   // 항목 삭제 함수
   const onRemove = useCallback(
-    id => {
-      setData({
-        ...data,
-        array: data.array.filter((info) => info.id !== id)
-      });
+    (id) => {
+      console.log("----onRemove :::::");
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1,
+          );
+        }),
+      );
     },
     [data],
   );
