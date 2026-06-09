67bb43f1a9955f71e3f4cf67ffe95c1e2bf53211
noris419 <noris419@naver.com>
2026-06-09 17:33:04 +0900

    ch17. Redux 사용 실습 end

--- Ch17/practice-redux/src/containers/TodosContainer.js
+++ Ch17/practice-redux/src/containers/TodosContainer.js
@@ -1,18 +1,19 @@
-import {  useDispatch, useSelector , shallowEqual  } from 'react-redux';
+import { useSelector , shallowEqual  } from 'react-redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, remove, toggle } from '../modules/todos';
-import { useCallback } from 'react';
+
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
     const { input , todos } = useSelector( ( { todos}) => ( { 
         input : todos.input , 
         todos : todos.todos 
     }), shallowEqual  ) ; 
-    const dispatch = useDispatch () ; 
-    const onChangeInput = useCallback ( input => dispatch(changeInput(input)), [dispatch]) ; 
-    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]) ; 
-    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]) ;
-    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]) ;
+
+    const [ onChangeInput , onInsert ,onToggle ,onRemove   ] = useActions(
+        [changeInput , insert , toggle , remove ], 
+        [] 
+    ); 
 
     return (
         <Todos
