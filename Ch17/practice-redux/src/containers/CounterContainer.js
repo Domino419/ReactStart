fd0aa0be1cb23a3e65a14de9b17c01fd1b81db48
noris419 <noris419@naver.com>
2026-06-09 15:42:04 +0900

    TodosContainer 만들기 end

--- Ch17/practice-redux/src/containers/CounterContainer.js
+++ Ch17/practice-redux/src/containers/CounterContainer.js
@@ -1,33 +1,37 @@
-
 import Counter from '../components/Counter';
 import { connect } from 'react-redux';
 import { decrease, increase } from '../modules/counter';
 
-const CounterContainer = ({ number, increase , decrease }) => {
-    return (
-        <div>
-      <Counter number={number} onIncrease={increase} onDecrease={decrease}/>      
-        </div>
-    );
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <div>
+      <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-    number : state.counter.number , 
-}) ; 
+// const mapStateToProps = state => ({
+//     number : state.counter.number ,
+// }) ;
 
-const mapDispatchToProps = dispatch => ( {
-    // 임시 함수 
-    increase : () => {
-        dispatch(increase() ) ; 
-        console.log('increase ') ; 
-    } , 
-    decrease : () => {
-        dispatch(decrease() ) ; 
-        console.log ('decrease') ; 
-    }, 
-}) ; 
+// const mapDispatchToProps = dispatch => ( {
+//     // 임시 함수
+//     increase : () => {
+//         dispatch(increase() ) ;
+//         console.log('increase ') ;
+//     } ,
+//     decrease : () => {
+//         dispatch(decrease() ) ;
+//         console.log ('decrease') ;
+//     },
+// }) ;
 
-export default connect ( 
-    mapStateToProps , 
-    mapDispatchToProps, 
-) (CounterContainer) ;
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);
