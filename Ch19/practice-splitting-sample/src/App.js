e9321a85fc9eb0824c1364c5b8e76834069df9ca
noris419 <noris419@naver.com>
2026-06-11 15:28:56 +0900

    Ch19.코드 스플리팅 개념 정리 end

--- Ch19/practice-splitting-sample/src/App.js
+++ Ch19/practice-splitting-sample/src/App.js
@@ -1,21 +1,30 @@
 import React, { useState, Suspense } from "react"; 
 import logo from "./logo.svg";
 import "./App.css";
+import loadable from '@loadable/component' ; 
 
-const SplitMe = React.lazy(() => import("./SplitMe"));
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback : <div> Loading... 로딩 중.....! </div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
 
   const onClick = () => {
     setVisible(true);
+    console.log( '클릭  ') ;
   };
+
+  const onMouseOver = () => {
+    SplitMe.preload() ; 
+    console.log( '마우스오버' ) ;
+  } ; 
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="app-logo" alt="logo"></img>
-        <p onClick={onClick}> Hello React ! </p>
+        <p onClick={onClick} onMouseOver={onMouseOver}> Hello React ! </p>
         <Suspense fallback={<div> loading...</div>}>
           {visible && <SplitMe />}
         </Suspense>
