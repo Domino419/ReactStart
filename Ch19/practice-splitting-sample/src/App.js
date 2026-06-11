300c5a8f60541bf5ac5c057101eaaee689e4a160
noris419 <noris419@naver.com>
2026-06-11 15:06:39 +0900

    스프리트 처리 전 백업

--- Ch19/practice-splitting-sample/src/App.js
+++ Ch19/practice-splitting-sample/src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+//import notify from "./notify";
 
 function App() {
+
+  const onClick = () => {
+    import('./notify').then(result => result.default() ) ; 
+    // notify() ;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="app-logo" alt="logo"></img>
-        <p> Hello React ! </p>
+        <p onClick={onClick}> Hello React !  </p>
       </header>
     </div>
   );
