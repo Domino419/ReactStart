b50e742a22d6b79158ee620849682e07ded6b9c2
noris419 <noris419@naver.com>
2026-06-15 09:30:57 +0900

    posts Router 생성 완료.

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,10 +1,9 @@
 // 21.5.4 라우트 모듈화
 
 const Router = require('koa-router');
-const api = new Router();
+const posts = require('./posts');
 
-api.get('/test', (ctx) => {
-  ctx.body = 'test 성공 ';
-});
+const api = new Router();
+api.use('/posts', posts.routes());
 
 module.exports = api;
