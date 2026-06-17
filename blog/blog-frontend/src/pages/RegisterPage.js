44d0b745149f16bf6f54883a5ef75899a61db8ad
noris419 <noris419@naver.com>
2026-06-17 11:23:14 +0900

    registerForm components add ;

--- blog/blog-frontend/src/pages/RegisterPage.js
+++ blog/blog-frontend/src/pages/RegisterPage.js
@@ -1,11 +1,12 @@
 import AuthTemplate from '../components/auth/AuthTemplate';
-import AuthForm from '../components/auth/AuthForm';
+//import AuthForm from '../components/auth/AuthForm';
+import RegisterForm from '../containers/auth/RegisterForm';
 
 const RegisterPage = () => {
   console.log('회원가입');
   return (
     <AuthTemplate>
-      <AuthForm type="register" />
+      <RegisterForm />
     </AuthTemplate>
   );
 };
