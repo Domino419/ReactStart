e235b65a0615c37efeeaea2956f076cab2c7e0aa
noris419 <noris419@naver.com>
2026-06-18 10:10:30 +0900

    component Header add

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
-import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const PostListPage = () => {
   console.log('포스트 리스트');
   return (
     <div>
-      <Button> 버튼 ! </Button>
+      <Header />
+      <div> 안녕하세요 - !!</div>
     </div>
   );
 };
