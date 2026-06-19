0827b5f9f51dccdaed1b2f245e946605452e2cba
noris419 <noris419@naver.com>
2026-06-19 13:07:48 +0900

    PostList UI add

--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,12 +1,13 @@
 import HeaderContainer from '../containers/common/HeaderContainer';
-import PostViewer from '../components/post/PostViewer';
+//import PostViewer from '../components/post/PostViewer';
+import PostViewerContainer from "../containers/post/PostViewerContainer";
 
 const PostPage = () => {
   console.log('포스트 읽기');
   return (
     <>
       <HeaderContainer />
-      <PostViewer />
+      <PostViewerContainer />
     </>
   );
 };
